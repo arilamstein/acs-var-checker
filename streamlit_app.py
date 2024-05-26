7f4af329074c4309fd239fa52ed58dd2a51faed6
Ari Lamstein <arilamstein@gmail.com>
2024-05-25 17:23:01 -0700

    Update footer with correct url for repo.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ var_name = st.text_input("ACS Variable Name: ", "B08006_017E")
                          
 st.dataframe(be.get_labels_for_variable(var_name), hide_index=True)
 
-st.write("Created by [Ari Lamstein](https://www.arilamstein.com). View the code [here](https://github.com/arilamstein/censusdis-streamlit).")
+st.write("Created by [Ari Lamstein](https://www.arilamstein.com). View the code [here](https://github.com/arilamstein/acs-var-checker).")
